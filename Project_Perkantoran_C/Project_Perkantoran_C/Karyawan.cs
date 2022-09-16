8b5d584e30924d37764fbdc5ddae12194f3fa276
HelloThePandaCorporation <113669297+HelloThePandaCorporation@users.noreply.github.com>
2022-09-16 17:34:46 +0700

    Add files via upload

---
+++ Project_Perkantoran_C/Project_Perkantoran_C/Karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Perkantoran_C
+{
+    class Karyawan
+    {
+        public string name,gender,phone;
+        public string alamat;
+
+
+        public  void add_info()
+        {
+            Console.WriteLine("Nama Karyawan :");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Jenis Kelamin :");
+            this.gender = Console.ReadLine();
+            Console.WriteLine("Alamat tinggal :");
+            this.alamat = Console.ReadLine();
+            Console.WriteLine("Nomor Telepon :");
+            this.phone = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Nama Karyawan    : "+ name);
+            Console.WriteLine("Jenis Kelamin    : "+ gender);
+            Console.WriteLine("Alamat tinggal   : "+ alamat);
+            Console.WriteLine("Nomor Telepon    : "+ phone);
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
---
+++ Project_Perkantoran_C/Project_Perkantoran_C/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Project_Perkantoran_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userinpute = "y";
+            do
+            {
+                Console.WriteLine("=====================================");
+                Console.WriteLine("Menu Perkantoran");
+                Console.WriteLine("1. Karyawan");
+                Console.WriteLine("2. Gaji");
+                Console.WriteLine("=====================================");
+                string userinput = Console.ReadLine();
+                switch (userinput)
+                {
+                    case "Karyawan":
+                        Karyawan tambah = new Karyawan();
+                        tambah.add_info();
+                        break;
+                    case "Gaji":
+                        Console.WriteLine("Masukkan Jumlah Gaji : ");
+                        string value1 = Console.ReadLine();
+                        Console.WriteLine("Masukkan Jumlah Bonus : ");
+                        string value2 = Console.ReadLine();
+                        Gaji penghasilan = new Gaji();
+                        double Hasil = penghasilan.total_pendapatan(Convert.ToDouble(value1), Convert.ToDouble(value2));
+                        Console.WriteLine("Total Penghasilan yang didapat = Rp. " + Hasil);
+                        break;
+                    default:
+                        Console.WriteLine("Mohon ketikkan dari pilihan 3 pilihan menu yang sudah ada");
+                        break;
+                }
+                Console.WriteLine("Apa anda ingin melanjutkan? y/t?");
+                userinpute = Console.ReadLine();
+            } while (userinpute == "y");
+        }
+    }
+
+    class Gaji
+    {
+        public double gaji_pokok, bonus;
+
+        public double total_pendapatan(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+    }
+    
+}
